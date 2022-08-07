f4f913d7edb0e1ea56aaaccca935cd83d4c662eb
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-07 11:56:11 +0300

    dwhvdwjdw djwdbwd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+import fetch from "node-fetch";
 app.use(express.json());
 app.use(cors());
 app.get("/doublon-enrollement", async (req, res) => {
