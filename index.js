e62b1e2d52e7d3a0ce416c4de684c080b1d18999
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-21 05:52:09 +0300

    nkn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require("express");
-
 const fetch = (...args) =>
   import("node-fetch").then(({ default: fetch }) => fetch(...args));
 var timeout = require("connect-timeout");
@@ -7,132 +6,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 app.get("/doublon-enrollment", async (req, res) => {
-  var query = req.query;
-  var username = query.username; //"Nosybe"
-  console.log(username);
-  var password = query.password; //"2021@Covax"
-  var periode = query.periode; //"LAST_12_MONTHS"
-  var idOrgUnit = query.idOrgUnit; //"A8UMJuP8iI3"
-  var sortie = query.sortie; //"enrollments"
-  var outputType = query.outputType; //"ENROLLMENT"
-  var sort = query.sort; //"enrollmentDate"
-  var columns =
-    "dimension=a1jCssI2LkW.eNRjVGxVL6l&dimension=a1jCssI2LkW.SB1IHYu2xQT&dimension=a1jCssI2LkW.NI0QRzJvQ0k&dimension=a1jCssI2LkW.LY2bDXpNvS7&dimension=a1jCssI2LkW.oindugucx72&dimension=a1jCssI2LkW.KSr2yTdu1AI&dimension=a1jCssI2LkW.Ewi7FUfcHAD&dimension=a1jCssI2LkW.fctSQp5nAYl";
-  var credentials = Buffer.from(username + ":" + password).toString("base64");
-  var auth = { Authorization: `Basic ${credentials}` };
-  var url = "https://covax.vaksiny.gov.mg/api/29/analytics/";
-  const response = await fetch(
-    URLStructure(url, sortie, periode, idOrgUnit, columns, outputType, sort),
-    { headers: auth }
-  );
-
-  var statusText = response.statusText;
-  var status = response.status;
-  if (status == "200") {
-    var data = await response.json();
-    var headerWidth = data.headerWidth;
-    var headers = [
-      "Unité d'organisation",
-      "Nom",
-      "Prénom",
-      "Date de naissance ",
-      "Type de cible",
-      "Sexe",
-      "CODE_EPI",
-      "CIN",
-      "TEL",
-    ];
-    var height = data.height;
-    var keys = [];
-    for (var i = 0; i < height; i++) {
-      keys.push(
-        (data.rows[i][10] + data.rows[i][11] + data.rows[i][12])
-          .replace(/\s/g, "")
-          .toUpperCase()
-      );
-    }
-    var duplicateKey = keys
-      .filter((item, index) => keys.indexOf(item) !== index)
-      .filter((n) => n);
-    var duplicateValue = [];
-    for (var i = 0; i < height; i++) {
-      if (
-        duplicateKey.includes(
-          (data.rows[i][10] + data.rows[i][11] + data.rows[i][12])
-            .replace(/\s/g, "")
-            .toUpperCase()
-        )
-      ) {
-        duplicateValue.push(data.rows[i]);
-      }
-    }
-    var sortedDuplicateValue = duplicateValue.sort((a, b) =>
-      (a[10] + a[11] + a[12]).replace(/\s/g, "").toUpperCase() >
-      (b[10] + b[11] + b[12]).replace(/\s/g, "").toUpperCase()
-        ? 1
-        : -1
-    );
-    var retour = [];
-    retour.push([]);
-    for (var i = 0; i < sortedDuplicateValue.length; i++) {
-      if (sortedDuplicateValue[i][13].replace(/\s/g, "").trim().length != 0) {
-        if (
-          parseInt(sortedDuplicateValue[i][13].replace(/\s/g, "").trim()) == 1
-        ) {
-          sortedDuplicateValue[i][13] = "Agent de santé";
-        }
-        if (
-          parseInt(sortedDuplicateValue[i][13].replace(/\s/g, "").trim()) == 2
-        ) {
-          sortedDuplicateValue[i][13] = "Force de l'ordre";
-        }
-        if (
-          parseInt(sortedDuplicateValue[i][13].replace(/\s/g, "").trim()) == 3
-        ) {
-          sortedDuplicateValue[i][13] = "Personne âgée";
-        }
-        if (
-          parseInt(sortedDuplicateValue[i][13].replace(/\s/g, "").trim()) == 4
-        ) {
-          sortedDuplicateValue[i][13] = "Travailleurs sociaux";
-        }
-        if (
-          parseInt(sortedDuplicateValue[i][13].replace(/\s/g, "").trim()) == 5
-        ) {
-          sortedDuplicateValue[i][13] = "Autres";
-        }
-      }
-
-      retour.push([
-        sortedDuplicateValue[i][7],
-        sortedDuplicateValue[i][10],
-        sortedDuplicateValue[i][11],
-        sortedDuplicateValue[i][12],
-        sortedDuplicateValue[i][13],
-        sortedDuplicateValue[i][14],
-        sortedDuplicateValue[i][15],
-        sortedDuplicateValue[i][16],
-        sortedDuplicateValue[i][17],
-      ]);
-    }
-
-    https: res.json({
-      statusText: statusText,
-      status: status,
-      data: retour,
-      headers: headers,
-    });
-  } else {
-    https: res.json({
-      statusText: statusText,
-      status: status,
-    });
-  }
-});
-
-app.get("/doublon-enrollment-2", async (req, res) => {
   var query = req.query;
   var username = query.username; //"Nosybe"
   console.log(username);
@@ -210,7 +84,7 @@ app.get("/doublon-enrollment-2", async (req, res) => {
         .replace(/\s/g, "")
         .toUpperCase()
     ) {
-      s.splice(s.length - 2, 1);
+      s.splice(s.length - 1, 1);
     }
 
     var retour = [];
