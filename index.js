cbe27dfb7572729849271b499ad351b75c9f3ec3
Tsanta <tsantaniainarakotonjanahary@gmail.com>
2022-08-22 06:12:55 +0300

    dwd

--- index.js
+++ index.js
@@ -85,23 +85,25 @@ app.get("/doublon-enrollment", async (req, res) => {
       ) {
         s.splice(i, 1);
         i = i - 1;
+      }
+    }
+
+    for (var i = 0; i < s.length; i++) {
+      if ((s[i][10] + s[i][11] + s[i][12]).replace(/\s/g, "").length != 0) {
+        s[i] = [
+          s[i][7],
+          s[i][10],
+          s[i][11],
+          s[i][12],
+          s[i][13],
+          s[i][14],
+          s[i][15],
+          s[i][16],
+          s[i][17],
+        ];
       } else {
-        if ((s[i][10] + s[i][11] + s[i][12]).replace(/\s/g, "").length != 0) {
-          s[i] = [
-            s[i][7],
-            s[i][10],
-            s[i][11],
-            s[i][12],
-            s[i][13],
-            s[i][14],
-            s[i][15],
-            s[i][16],
-            s[i][17],
-          ];
-        } else {
-          s.splice(i, 1);
-          i = i - 1;
-        }
+        s.splice(i, 1);
+        i = i - 1;
       }
     }
 
