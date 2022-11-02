6ce5715401540ea1dfb6a447577da5aa7bf59422
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2022-11-02 05:11:07 +0300

    go

--- index.js
+++ index.js
@@ -230,7 +230,6 @@ app.get("/centrel6Byl5mongo", async (req,res) => res.json(await db.collection('c
 */
 
 app.get("premierDose", async (req,res)=> {
-
   const response = await fetch("https://covax.vaksiny.gov.mg/api/29/analytics.json?dimension=dx:cNx2l3Lfw7A&dimension=pe:THIS_YEAR;LAST_5_YEARS&filter=ou:"+req.query.ou+"&displayProperty=NAME",
     {
       headers: {
@@ -240,9 +239,8 @@ app.get("premierDose", async (req,res)=> {
       },
     }
   );
-  var s = (await response.json());
+  var s = await response.json();
   res.json(s);
-
 })
 
 
